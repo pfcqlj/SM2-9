7a4091e2ce7b2c2fd50aab557c3df588c8eb8b18
pfcqlj <39401686+pfcqlj@users.noreply.github.com>
2023-01-12 17:10:46 +0800

    Update sm2.go
    add

--- cryptogm/sm/sm2/sm2.go
+++ cryptogm/sm/sm2/sm2.go
@@ -7,8 +7,8 @@ package sm2
 
 import (
 	"crypto"
-	"github.com/xlcetc/cryptogm/sm/sm3"
-	"github.com/xlcetc/cryptogm/elliptic/sm2curve"
+	"github.com/pfcqlj/SM2-9/cryptogm/sm/sm3"
+	"github.com/pfcqlj/SM2-9/cryptogm/elliptic/sm2curve"
 	"errors"
 	"io"
 	"math/big"
